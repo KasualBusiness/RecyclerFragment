ad6f7816237673658ae8d86fc659facb902fab5b
Stephen Vinouze <s.vinouze@gmail.com>
2015-11-09 16:57:42 +0100

    [*] Add ripple drawable to cards

--- recyclerfragment/src/main/java/biz/kasual/recyclerfragment/views/BasicCardView.java
+++ recyclerfragment/src/main/java/biz/kasual/recyclerfragment/views/BasicCardView.java
@@ -1,8 +1,12 @@
 package biz.kasual.recyclerfragment.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+
+import biz.kasual.recyclerfragment.R;
 
 /**
  * Created by Stephen Vinouze on 09/11/2015.
@@ -23,6 +27,12 @@ public class BasicCardView extends CardView {
         setUseCompatPadding(true);
         setCardElevation(8);
         setRadius(5);
+
+        int[] attrs = new int[] { R.attr.selectableItemBackground};
+        TypedArray ta = getContext().obtainStyledAttributes(attrs);
+        Drawable drawableFromTheme = ta.getDrawable(0);
+        setForeground(drawableFromTheme);
+        ta.recycle();
     }
 
 }
